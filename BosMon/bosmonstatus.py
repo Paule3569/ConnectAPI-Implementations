31ff2958e6731c8dba409093c29ab5a114921d92
bouni <bouni@owee.de>
2018-06-04 17:44:21 +0200

    Update bosmonstatus.py

--- BosMon/bosmonstatus.py
+++ BosMon/bosmonstatus.py
@@ -2,7 +2,8 @@
 #-*- coding: utf-8 -*-
 
 import os
-import socket
+import re
+import socketserver
 import requests
 import json
 from datetime import datetime as dt
@@ -32,9 +33,43 @@ class ConnectAPI:
         if r.status_code != 200:
             print(f"Fehler beim senden des Alarms {r.status_code}, \"{r.text}\"")
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print(f"Fehler, Falsche Anzahl an Argumenten!")
-    api = ConnectAPI()
-    api.set_status(sys.argv[1], sys.argv[2])
+
+
+
+class BosMonTCPHandler(socketserver.BaseRequestHandler):
     
+    def handle(self):
+        data = self.request.recv(1024).strip().decode("utf-8")
+        print(data)
+        address = re.search("<address>(?P<address>.*)<\/address>", data)
+        if address:
+            address = address.groupdict().get("address", None)
+        type = re.search("<type>(?P<type>.*)<\/type>", data)
+        if type:
+            type = type.groupdict().get("type", None)
+        timestamp = re.search("<timestamp>(?P<timestamp>.*)<\/timestamp>", data)
+        if timestamp:
+            timestamp = timestamp.groupdict().get("timestamp", None)
+        flags = re.search("<flags>(?P<flags>.*)<\/flags>", data)
+        if flags:
+            flags = flags.groupdict().get("flags", None)
+        status = re.search("<status>(?P<status>.*)<\/status>", data)
+        if status:
+            status = status.groupdict().get("status", None)
+        direction = re.search("<direction>(?P<direction>.*)<\/direction>", data)
+        if direction:
+            direction = direction.groupdict().get("direction", None)
+        shortdescription = re.search("<shortdescription>(?P<shortdescription>.*)<\/shortdescription>", data)
+        if shortdescription:
+            shortdescription = shortdescription.groupdict().get("shortdescription", None)
+        longdescription = re.search("<longdescription>(?P<longdescription>.*)<\/longdescription>", data)
+        if longdescription:
+            longdescription = longdescription.groupdict().get("longdescription", None)
+        api = ConnectAPI()
+        api.set_status(address, status)
+        
+        
+
+if __name__ == "__main__":
+    server = socketserver.TCPServer(("127.0.0.1", 5555), BosMonTCPHandler)
+    server.serve_forever()
